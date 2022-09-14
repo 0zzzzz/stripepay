14660a56eb11e157216d3f5d60b24a7e929f65e7
ozzzzz <you@example.com>
2022-09-14 17:43:30 +0300

    order model add

--- goodsapp/admin.py
+++ goodsapp/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, Order, OrderItem
+
+
+class OrderItemAdmin(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemAdmin]
+
 
 admin.site.register(Item)
+admin.site.register(Order, OrderAdmin)
--- stripepay/urls.py
+++ stripepay/urls.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from django.urls import path
-from goodsapp.views import CancelView, SuccessView, CreateCheckoutSessionAPIView, ItemBuyAPIView
+from goodsapp.views import (
+    CancelView,
+    SuccessView,
+    CreateCheckoutSessionAPIView,
+    ItemBuyAPIView,
+    CreateCheckoutSessionFromOrderAPIView,
+    OrderBuyAPIView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cancel/', CancelView.as_view(), name='cancel'),
     path('success/', SuccessView.as_view(), name='success'),
     path('item/<int:pk>/', ItemBuyAPIView.as_view(), name='item_by'),
+    path('order/<int:pk>/', OrderBuyAPIView.as_view(), name='item_by'),
     path('buy/<int:pk>/', CreateCheckoutSessionAPIView.as_view(), name='create-checkout-session'),
+    path('buy_order/<int:pk>/', CreateCheckoutSessionFromOrderAPIView.as_view(), name='create-checkout-session-order'),
 ]
